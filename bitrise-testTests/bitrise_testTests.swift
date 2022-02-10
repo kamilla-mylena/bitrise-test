984376e3952c64eebcaaf725a52c1b2619c788bb
kamilla-mylena <kamilla.antunes@ciandt.com>
2022-02-10 15:08:07 -0300

    commit: tests

--- bitrise-testTests/bitrise_testTests.swift
+++ bitrise-testTests/bitrise_testTests.swift
@@ -21,13 +21,11 @@ class bitrise_testTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssert(false)
     }
 
 }
